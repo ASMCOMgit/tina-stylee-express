b6196478449b5493ae7693ea332fe634b7068789
ASMCOMgit <asmcom@hotmail.com>
2025-05-14 16:40:13 -0300

    Update login.js

--- api/login.js
+++ api/login.js
@@ -3,6 +3,6 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 export default function loginHandler(req, res) {
-  const redirectUrl = \`https://accounts.tiny.com.br/realms/tiny/protocol/openid-connect/auth?client_id=\${process.env.TINY_CLIENT_ID}&redirect_uri=\${process.env.TINY_REDIRECT_URI}&response_type=code&scope=openid\`;
+  const redirectUrl = `https://accounts.tiny.com.br/realms/tiny/protocol/openid-connect/auth?client_id=${process.env.TINY_CLIENT_ID}&redirect_uri=${process.env.TINY_REDIRECT_URI}&response_type=code&scope=openid`;
   res.redirect(redirectUrl);
 }
